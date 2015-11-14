7c1635d6bbd476c3347e6029f28e86afc78bbcd4
SkyTheCoder <davidac2001@gmail.com>
2015-11-13 20:14:13 -0800

    Create KeyHandler.java

---
+++ KeyHandler.java
@@ -0,0 +1,23 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+}
